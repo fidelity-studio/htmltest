5263cf6ebf0bbec96de11e9078bf147be2b773ed
fidelity-studio <129822506+fidelity-studio@users.noreply.github.com>
2025-03-26 08:18:03 +0900

    Add files via upload

--- assets/js/fade.js
+++ assets/js/fade.js
@@ -1,32 +1,21 @@
 const elements = document.querySelectorAll('[data-fade="true"]');
 
 window.addEventListener('scroll', () => {
-  elements.forEach(element => {
-    const rect = element.getBoundingClientRect(); 
-    const windowWidth = window.innerWidth; 
-    const windowHeight = window.innerHeight; 
-
-
-    const elementCenterX = rect.left + rect.width / 2;
-    const elementCenterY = rect.top + rect.height / 2;
-
-
-    const screenCenterX = windowWidth / 2;
-    const screenCenterY = windowHeight / 2;
-
-
-    const distanceX = Math.abs(screenCenterX - elementCenterX); 
-    const distanceY = Math.abs(screenCenterY - elementCenterY); 
-
-    const maxDistance = Math.sqrt(Math.pow(windowWidth / 2, 2) + Math.pow(windowHeight / 2, 2)); 
-    const distanceFromCenter = Math.sqrt(Math.pow(distanceX, 2) + Math.pow(distanceY, 2)); 
-
-    const brightness = Math.max(1 - Math.pow(distanceFromCenter / maxDistance, 5), 0.45); 
-
-    element.style.filter = `brightness(${brightness})`;
+  elements.forEach((element) => {
+    const rect = element.getBoundingClientRect();
+    const windowHeight = window.innerHeight;
+    const screenQuarter = windowHeight / 4;
+    const elementStart = rect.top;
+    const elementEnd = rect.bottom;
+    if (elementStart < screenQuarter && elementEnd > screenQuarter) {
+      element.style.filter = `brightness(1)`; 
+    } else {
+      element.style.filter = `brightness(0.5)`; 
+    }
   });
 });
 
-window.addEventListener('DOMContentLoaded', updateBrightness);
-
-window.addEventListener('scroll', updateBrightness);
+window.addEventListener('DOMContentLoaded', () => {
+  const event = new Event('scroll');
+  window.dispatchEvent(event);
+});
