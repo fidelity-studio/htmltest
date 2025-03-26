e2651966f0ffa01eda4dac4692a887648916d5d5
fidelity-studio <129822506+fidelity-studio@users.noreply.github.com>
2025-03-26 21:33:14 +0900

    Add files via upload

--- assets/js/fade.js
+++ assets/js/fade.js
@@ -4,13 +4,12 @@ window.addEventListener('scroll', () => {
   elements.forEach((element) => {
     const rect = element.getBoundingClientRect();
     const windowHeight = window.innerHeight;
-    const screenQuarter = windowHeight / 4;
-    const elementStart = rect.top;
-    const elementEnd = rect.bottom;
-    if (elementStart < screenQuarter && elementEnd > screenQuarter) {
+    
+    // 요소의 중간 지점이 화면의 상단 1/4 ~ 하단 1/4 사이에 있으면 밝기 조절
+    if (rect.top < windowHeight * 3/4 && rect.bottom > windowHeight / 4) {
       element.style.filter = `brightness(1)`; 
     } else {
-      element.style.filter = `brightness(0.5)`; 
+      element.style.filter = `brightness(0.6)`; 
     }
   });
 });
